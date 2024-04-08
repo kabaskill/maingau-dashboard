4c8574cba6ec1ae826eb6c23d0b4ff4c93e6583c
kabaskill <oguzkabasakal@gmail.com>
2024-04-08 03:35:55 +0200

    update welcome banner and header

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,7 +19,6 @@ import DashboardCard10 from '../partials/dashboard/DashboardCard10';
 import DashboardCard11 from '../partials/dashboard/DashboardCard11';
 import DashboardCard12 from '../partials/dashboard/DashboardCard12';
 import DashboardCard13 from '../partials/dashboard/DashboardCard13';
-import Banner from '../partials/Banner';
 
 function Dashboard() {
 
@@ -100,9 +99,6 @@ function Dashboard() {
 
           </div>
         </main>
-
-        <Banner />
-
       </div>
     </div>
   );
