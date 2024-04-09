ed4e1554899fa0e69fd3c17fe85534340ac32a1d
kabaskill <oguzkabasakal@gmail.com>
2024-04-09 02:08:59 +0200

    add correct links to header

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -60,7 +60,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         ref={sidebar}
         className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 p-2 transition-all duration-200 ease-in-out ${
           sidebarOpen ? "translate-x-0" : "-translate-x-64"
-        }  bg-sky-100 text-[var(--color-text-blue)] `}
+        }  bg-sky-100 text-[var(--color-text-blue)] dark:bg-slate-800 dark:text-white`}
       >
         {/* Sidebar header */}
         <div className="flex flex-col gap-2 mb-8 sm:px-2 ">
