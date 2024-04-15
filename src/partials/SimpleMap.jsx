e6e4cdd0609fde3eab7f9fd71beaaf201192d41d
kabaskill <oguzkabasakal@gmail.com>
2024-04-15 14:06:56 +0200

    fix map marker

--- src/partials/SimpleMap.jsx
+++ src/partials/SimpleMap.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 
 delete L.Icon.Default.prototype._getIconUrl;
