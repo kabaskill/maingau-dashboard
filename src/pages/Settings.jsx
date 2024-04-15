9048c1469fe9c8f826c76fa90f79c762e27ede95
kabaskill <oguzkabasakal@gmail.com>
2024-04-15 18:28:01 +0200

    style settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,10 @@
 import useUser from "../utils/useUser";
+import { useState } from "react";
 import WelcomeBanner from "../partials/WelcomeBanner";
 
 function Settings() {
   const { user, error, isLoading } = useUser(0);
+  const [userData, setUserData] = useState(user);
 
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading.....</div>;
@@ -15,16 +17,22 @@ function Settings() {
     console.log("🚀  updatedUserData:", updatedUserData);
 
     try {
-      const response = await fetch("http://localhost:3000/data/", {
+      const response = await fetch("http://localhost:3000/api/users", {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(updatedUserData),
+        body: JSON.stringify({
+          ...user,
+          ...updatedUserData,
+        }),
       });
 
       if (response.ok) {
-        setUserData(updatedUserData);
+        setUserData({
+          ...user,
+          ...updatedUserData,
+        });
         alert("Data updated successfully");
       } else {
         alert("Failed to update data");
@@ -33,113 +41,144 @@ function Settings() {
       console.error("Error updating data:", error);
     }
   };
-
   return (
     <main className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto b">
       <WelcomeBanner />
 
       <form
         onSubmit={handleSubmit}
-        className="  bg-white dark:bg-slate-700  flex flex-col gap-2 p-8"
+        className="  bg-white dark:bg-slate-700  w-[80%] grid grid-cols-2 gap-10 p-8 mx-auto "
       >
-        <label htmlFor="vorname">Vorname</label>
-        <input
-          className="form-input "
-          type="text"
-          id="vorname"
-          name="vorname"
-          placeholder="Vorname"
-          defaultValue={user.vorname}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="vorname">Vorname</label>
+          <input
+            className="form-input"
+            type="text"
+            id="vorname"
+            name="vorname"
+            placeholder="Vorname"
+            defaultValue={user.vorname}
+          />
+        </div>
 
-        <label htmlFor="nachname">Nachname</label>
-        <input
-          className="form-input "
-          type="text"
-          id="nachname"
-          name="nachname"
-          placeholder="Nachname"
-          defaultValue={user.nachname}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="nachname">Nachname</label>
+          <input
+            className="form-input"
+            type="text"
+            id="nachname"
+            name="nachname"
+            placeholder="Nachname"
+            defaultValue={user.nachname}
+          />
+        </div>
 
-        <label htmlFor="email">E-mail</label>
-        <input
-          className="form-input"
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          defaultValue={user.email}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="email">E-mail</label>
+          <input
+            className="form-input"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            defaultValue={user.email}
+          />
+        </div>
 
-        <label htmlFor="password">Password</label>
-        <input
-          className="form-input"
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          defaultValue={user.password}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="password">Password</label>
+          <input
+            className="form-input"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            defaultValue={user.password}
+          />
+        </div>
 
-        <label htmlFor="birthdate">Geburtstag</label>
-        <input
-          className="form-input"
-          type="date"
-          id="birthdate"
-          name="birthdate"
-          placeholder="Birth Date"
-          defaultValue={user.geburtstag}
-          min="1950-01-01"
-          max="2024-12-31"
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="birthdate">Geburtstag</label>
+          <input
+            className="form-input"
+            type="date"
+            id="birthdate"
+            name="birthdate"
+            placeholder="Birth Date"
+            defaultValue={user.geburtstag}
+            min="1950-01-01"
+            max="2024-12-31"
+          />
+        </div>
 
-        <label htmlFor="hausnummer">Hausnummer</label>
-        <input
-          className="form-input"
-          type="number"
-          id="hausnummer"
-          name="hausnummer"
-          placeholder="Hausnummer"
-          defaultValue={user.hausnummer}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="telefon">Telefon</label>
+          <input
+            className="form-input"
+            type="text"
+            id="telefon"
+            name="telefon"
+            placeholder="Telefon"
+            defaultValue={user.telefon}
+          />
+        </div>
 
-        <label htmlFor="street">Street</label>
-        <input
-          className="form-input"
-          type="text"
-          id="street"
-          name="street"
-          placeholder="Street"
-          defaultValue={user.strasse}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="street">Street</label>
+          <input
+            className="form-input"
+            type="text"
+            id="street"
+            name="street"
+            placeholder="Street"
+            defaultValue={user.strasse}
+          />
+        </div>
 
-        <label htmlFor="city">City</label>
-        <input
-          className="form-input"
-          type="text"
-          id="city"
-          name="city"
-          placeholder="City"
-          defaultValue={user.city}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="hausnummer">Hausnummer</label>
+          <input
+            className="form-input"
+            type="number"
+            id="hausnummer"
+            name="hausnummer"
+            placeholder="Hausnummer"
+            defaultValue={user.hausnummer}
+          />
+        </div>
 
-        <label htmlFor="plz">Postleitzahl (PLZ)</label>
-        <input
-          className="form-input"
-          type="number"
-          id="plz"
-          name="plz"
-          placeholder="Postleitzahl"
-          defaultValue={user.plz}
-        />
+        <div className="flex flex-col ">
+          <label htmlFor="city">City</label>
+          <input
+            className="form-input"
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City"
+            defaultValue={user.city}
+          />
+        </div>
 
-        <button
-          className="btn-lg bg-[var(--color-btn-blue)] text-white hover:bg-[var(--color-dark-blue)] mt-4 w-1/3 self-center"
-          type="submit"
-        >
-          Save Changes
-        </button>
+        <div className="flex flex-col ">
+          <label htmlFor="plz">Postleitzahl (PLZ)</label>
+          <input
+            className="form-input"
+            type="number"
+            id="plz"
+            name="plz"
+            placeholder="Postleitzahl"
+            defaultValue={user.plz}
+          />
+        </div>
+
+        <div className="col-span-2 flex justify-center">
+          <button
+            className="btn-lg bg-[var(--color-btn-blue)] text-white hover:bg-[var(--color-dark-blue)] mt-4 w-1/3 "
+            type="submit"
+          >
+            Save Changes
+          </button>
+        </div>
       </form>
     </main>
   );
