1e6102f74232fa915469e006789b276dd2ab3da0
kabaskill <oguzkabasakal@gmail.com>
2024-04-15 20:00:02 +0200

    responsive style updates

--- src/pages/RegionCheck.jsx
+++ src/pages/RegionCheck.jsx
@@ -11,6 +11,10 @@ function RegionCheck() {
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading.....</div>;
 
+  function handleRequestButton() {
+    alert("Request for your address sent successfully!");
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -39,19 +43,19 @@ function RegionCheck() {
   };
 
   return (
-    <main className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto b">
+    <main className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
       <WelcomeBanner />
 
-      <section className="flex gap-4">
+      <section className="flex flex-col lg:flex-row lg:space-x-8">
         <form
-          className="  bg-white dark:bg-slate-700  grid grid-cols-[2fr_1fr] gap-6 p-8 w-2/5"
+          className="bg-white dark:bg-slate-700 p-8 lg:w-1/3"
           onSubmit={handleSubmit}
         >
-          <h3 className="text-2xl dark:text-white font-extrabold text-[var(--color-text-blue)] col-span-full mb-4">
+          <h3 className="text-2xl dark:text-white font-extrabold text-[var(--color-text-blue)] mb-4">
             Check Glasfaser Availability
           </h3>
 
-          <div className="flex flex-col  gap-2">
+          <div className="flex flex-col gap-2">
             <label htmlFor="street">Street</label>
             <input
               className="form-input"
@@ -99,22 +103,25 @@ function RegionCheck() {
             />
           </div>
 
-          <button
-            className="btn-lg text-[var(--color-text-blue)] text-lg border-1 border-black hover:bg-[var(--color-dark-blue)]  col-span-2 mt-8 mb-2"
-            type="button"
-          >
-            Make a Request
-          </button>
+          <div className="flex justify-between mt-8">
+            <button
+              className="btn-lg text-[var(--color-text-blue)] border-1 border-black hover:bg-[var(--color-dark-blue)]"
+              type="button"
+              onClick={handleRequestButton}
+            >
+              Make a Request
+            </button>
 
-          <button
-            className="btn-lg bg-[var(--color-btn-blue)] text-white text-lg hover:bg-[var(--color-dark-blue)]  col-span-2  mb-2"
-            type="submit"
-          >
-            Check Availability
-          </button>
+            <button
+              className="btn-lg bg-[var(--color-btn-blue)] text-white hover:bg-[var(--color-dark-blue)]"
+              type="submit"
+            >
+              Check Availability
+            </button>
+          </div>
         </form>
 
-        <div id="map" className="w-3/5 ">
+        <div className="lg:w-2/3 mt-8 lg:mt-0">
           <SimpleMap posX={coordinates.posX} posY={coordinates.posY} />
         </div>
       </section>
--- src/pages/Tariffs.jsx
+++ src/pages/Tariffs.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import WelcomeBanner from "../partials/WelcomeBanner";
 
 function Tariffs() {
@@ -5,18 +6,24 @@ function Tariffs() {
     <main className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto b">
       <WelcomeBanner />
 
-      <section className="grid grid-cols-3">
-        <a href="https://www.maingau-energie.de/mobilfunk/5G-Handytarife">
-          <img src="/images/5g.png" alt="mobilfunk-5g" />
-        </a>
+      <section className="flex flex-wrap justify-center lg:justify-start">
+        <div className="w-full lg:w-1/3 px-2 mb-4 lg:mb-0 ">
+          <Link to="https://www.maingau-energie.de/mobilfunk/5G-Handytarife" className="">
+            <img src="/images/5g.png" alt="mobilfunk-5g" />
+          </Link>
+        </div>
 
-        <a href="https://www.maingau-energie.de/internet/glasfaser">
-          <img src="/images/glasfaser.png" alt="glasfaser" />
-        </a>
+        <div className="w-full lg:w-1/3 px-2 mb-4 lg:mb-0 ">
+          <Link to="https://www.maingau-energie.de/internet/glasfaser" className="">
+            <img src="/images/glasfaser.png" alt="glasfaser" />
+          </Link>
+        </div>
 
-        <a href="https://www.maingau-energie.de/e-mobilitaet">
-          <img src="/images/autostrom.png" alt="autostrom" />
-        </a>
+        <div className="w-full lg:w-1/3 px-2 mb-4 lg:mb-0 ">
+          <Link to="https://www.maingau-energie.de/e-mobilitaet" className="">
+            <img src="/images/autostrom.png" alt="autostrom" />
+          </Link>
+        </div>
       </section>
     </main>
   );
